93d9d4f4b0f0c4e15d2a67613baf75bef8f49380
James Beavers <james.beavers@inin.com>
2016-08-25 12:55:41 -0400

    Advertise IP addresses that the host can see so cluster client can switch slots/nodes

--- docker-data/start.sh
+++ docker-data/start.sh
@@ -1,6 +1,7 @@
 supervisord
 sleep 3
 
-IP=`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
+# redis cluster doesn't play well with docker (or other NAT'd environments), have to explicitly advertise externally routable IP
+IP=`ifconfig eth1 | grep "inet addr:" | cut -f2 -d ":" | cut -f1 -d " "`
 echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
 tail -f /var/log/supervisor/redis-1.log
