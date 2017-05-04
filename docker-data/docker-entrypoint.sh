34a17b1be6909cf9c5aff1396aeab1cf5aa4d72b
Patrick Ting <pcting@gmail.com>
2017-05-04 10:06:23 -0700

    Reduce Cluster Size To 3

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = 'redis-cluster' ]; then
-    for port in `seq 7000 7007`; do
+    for port in `seq 7001 7003`; do
       mkdir -p /redis-conf/${port}
       mkdir -p /redis-data/${port}
 
@@ -10,19 +10,15 @@ if [ "$1" = 'redis-cluster' ]; then
       fi
     done
 
-    for port in `seq 7000 7005`; do
+    for port in `seq 7001 7003`; do
       PORT=${port} envsubst < /redis-conf/redis-cluster.tmpl > /redis-conf/${port}/redis.conf
-    done
-
-    for port in `seq 7006 7007`; do
-      PORT=${port} envsubst < /redis-conf/redis.tmpl > /redis-conf/${port}/redis.conf
     done
 
     supervisord -c /etc/supervisor/supervisord.conf
     sleep 3
 
     IP=`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
-    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
+    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7001 ${IP}:7002 ${IP}:7003
     tail -f /var/log/supervisor/redis*.log
 else
   exec "$@"
