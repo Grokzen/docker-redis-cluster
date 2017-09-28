8150c2cde6f11fd8ae88022350c7d1ce2e3d2a9f
theyough <legleuthugo@gmail.com>
2017-09-28 17:58:55 +0100

    Fix: Mac osx and redis doesn't support NAT

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -17,7 +17,7 @@ if [ "$1" = 'redis-cluster' ]; then
     supervisord -c /redis-conf/supervisord.conf
     sleep 3
 
-    IP=`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
+    IP=`ifconfig | grep "inet addr:127" | cut -f2 -d ":" | cut -f1 -d " "`
     echo "yes" | ruby /redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
     tail -f /var/log/supervisor/redis*.log
 else
