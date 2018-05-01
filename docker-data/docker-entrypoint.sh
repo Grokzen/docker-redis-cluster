e0f104f2912d87d6d71e33f11d65a51b076f7276
Chris Scutcher <chris@scutcher.uk>
2018-05-01 15:39:49 +0100

    Revert "Fix the ip problem on MaxOs"
    This reverts commit ccd7f28b7456a3c2dc3c6e3cdd3860509e6708b3.

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -27,7 +27,6 @@ if [ "$1" = 'redis-cluster' ]; then
     sleep 3
 
     IP=`ifconfig | grep "inet addr:17" | cut -f2 -d ":" | cut -f1 -d " "`
-    IP=0.0.0.0
     echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
     tail -f /var/log/supervisor/redis*.log
 else
