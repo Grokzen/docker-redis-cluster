423a0e4ff5473ca2f84d0107ba2cbc13491c2e1a
Bogomolov Ivan <ibogomolov@arello-mobile.com>
2018-09-14 12:55:39 +0700

    Use hostname instead of regexp to discover IP

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -29,10 +29,11 @@ if [ "$1" = 'redis-cluster' ]; then
     supervisord -c /etc/supervisor/supervisord.conf
     sleep 3
 
-    # If IP is unset then discover it
-    if [ -z "$IP" ]; then
-        IP=$(ifconfig | grep -P "\s+inet 17" | awk -F ' ' '{print $2}')
+    if [ -z "$IP" ]; then # If IP is unset then discover it
+        IP=$(hostname -I)
     fi
+    IP=$(echo ${IP}) # trim whitespaces
+
     echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 ${IP}:7000 ${IP}:7001 ${IP}:7002 ${IP}:7003 ${IP}:7004 ${IP}:7005
     tail -f /var/log/supervisor/redis*.log
 else
