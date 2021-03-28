a034d2df3c57aca727260097f8f9d323aa56d0b1
Grokzen <Grokzen@gmail.com>
2021-03-28 21:05:18 +0200

    Add in latest redis version on 5.0, 6.0, 6.2 version tracks. Simplified the version string generation. Minor documentation tweaks.

--- tasks.py
+++ tasks.py
@@ -3,79 +3,16 @@ from multiprocessing import Pool
 from invoke import task
 
 
-latest_version_string = "6.2.0"
+latest_version_string = "6.2.1"
 
-version_config_mapping = [
-    "3.0.0",
-    "3.0.1",
-    "3.0.2",
-    "3.0.3",
-    "3.0.4",
-    "3.0.5",
-    "3.0.6",
-    "3.0.7",
-
-    "3.2.0",
-    "3.2.1",
-    "3.2.2",
-    "3.2.3",
-    "3.2.4",
-    "3.2.5",
-    "3.2.6",
-    "3.2.7",
-    "3.2.8",
-    "3.2.9",
-    "3.2.10",
-    "3.2.11",
-    "3.2.12",
-    "3.2.13",
-
-    "4.0.0",
-    "4.0.1",
-    "4.0.2",
-    "4.0.3",
-    "4.0.4",
-    "4.0.5",
-    "4.0.6",
-    "4.0.7",
-    "4.0.8",
-    "4.0.9",
-    "4.0.10",
-    "4.0.11",
-    "4.0.12",
-    "4.0.13",
-    "4.0.14",
-
-    "5.0.0",
-    "5.0.1",
-    "5.0.2",
-    "5.0.3",
-    "5.0.4",
-    "5.0.5",
-    "5.0.6",
-    "5.0.7",
-    "5.0.8",
-    "5.0.9",
-    "5.0.10",
-    "5.0.11",
-
-    "6.0.0",
-    "6.0.1",
-    "6.0.2",
-    "6.0.3",
-    "6.0.4",
-    "6.0.5",
-    "6.0.6",
-    "6.0.7",
-    "6.0.8",
-    "6.0.9",
-    "6.0.10",
-    "6.0.11",
-
-    "6.2-rc1",
-    "6.2-rc2",
-    "6.2.0",
-]
+version_config_mapping = []
+version_config_mapping += [f"3.0.{i}" for i in range(0, 8)]
+version_config_mapping += [f"3.2.{i}" for i in range(0, 14)]
+version_config_mapping += [f"4.0.{i}" for i in range(0, 15)]
+version_config_mapping += [f"5.0.{i}" for i in range(0, 13)]
+version_config_mapping += [f"6.0.{i}" for i in range(0, 13)]
+version_config_mapping += [f"6.2-rc{i}" for i in range(1, 3)]
+version_config_mapping += [f"6.2.{i}" for i in range(0, 2)]
 
 
 def version_name_to_version(version):
@@ -178,7 +115,7 @@ def push(c, version, cpu=None):
 
     pool = Pool(get_pool_size(cpu))
     pool.map(
-        _docker_build,
+        _docker_push,
         [
             [c, version]
             for version in version_name_to_version(version)
